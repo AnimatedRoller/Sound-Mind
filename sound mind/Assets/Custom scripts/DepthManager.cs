10b384be19f7e63c4e50599f2a42d961d9750546
AnimatedRoller <92384941+AnimatedRoller@users.noreply.github.com>
2022-05-10 08:07:58 +0100

    fixed scripting issues

--- sound mind/Assets/Custom scripts/DepthManager.cs
+++ sound mind/Assets/Custom scripts/DepthManager.cs
@@ -7,16 +7,21 @@ public class DepthManager : MonoBehaviour
     static int depth = 0;
     static int sanity = 100;
     static int infoGathered = 0;
+    GameObject darkness = GameObject.Find("DarknessOverlay");
+    //*Color tmp = darkness.GetComponent<SpriteRenderer>().color;
+    //*tmp.a = 0f;
 
     void Start()
     {
-        InvokeRepeating("depthChange", 60, 60);
+        InvokeRepeating("depthChange", 10, 10);
         InvokeRepeating("sanityChange", 30, 30);
     }
     void depthChange()
     {
         depth += 10;
         Debug.Log(depth.ToString());
+        //*tmp.a += 5f;
+        //*darkness.GetComponent<SpriteRenderer>().color = tmp;
     }
     void sanityChange()
     {
